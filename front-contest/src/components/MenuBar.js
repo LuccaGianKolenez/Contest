1702dbf93c6c09e79d94723316452fd442695d9f
Lucca Gian Kolenez <93713182+LuccaGianKolenezBPlus@users.noreply.github.com>
2023-09-12 20:34:21 -0300

    Código básico front

---
+++ front-contest/src/components/MenuBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css'; 
+
+function MenuBar() {
+  return (
+    <div className="menu-bar">
+      <ul>
+        <li><a href="/">AAAA</a></li>
+        <li><a href="/BBBB">BBBB</a></li>
+        <li><a href="/CCCC">CCCC</a></li>
+        <li><a href="/DDDD">DDDD</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default MenuBar;
