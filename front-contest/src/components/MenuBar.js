2cf07f8d5e3e7e0f9e1d55e28f1ea55c10b08236
Lucca Gian Kolenez <93713182+LuccaGianKolenezBPlus@users.noreply.github.com>
2023-09-13 19:02:28 -0300

    Adicionar API
    Necessário reinstalar, pois, adicionei a biblioteca do Axios

--- front-contest/src/components/MenuBar.js
+++ front-contest/src/components/MenuBar.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, {/*useState,*/ useCallback} from 'react';
 import './style.css'; 
 
+//Importar api para fazer uma chamada, get, post, put, delete ...
+import api from '../services/index.js'
+
 function MenuBar() {
+
+//Setar algum valor futuramente
+ //const [value, setValue] = useState('');
+
+ //Exemplo de chamada ao backend, por enquanto não busca nada
+  const chamadaBack = useCallback(
+    async (value) => {
+      await api.post("/teste", {value: "value"})
+        .then((res) => {
+          console.log(res.data)
+
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+    []
+  );
+
+  //Menu básico e botão para executar a chamada a API, somente para testes
   return (
+    <>
     <div className="menu-bar">
       <ul>
-        <li><a href="/">AAAA</a></li>
-        <li><a href="/BBBB">BBBB</a></li>
-        <li><a href="/CCCC">CCCC</a></li>
-        <li><a href="/DDDD">DDDD</a></li>
+        <li><a href="/">Função A</a></li>
+        <li><a href="/BBBB">Função B</a></li>
+        <li><a href="/CCCC">Função C</a></li>
+        <li><a href="/DDDD">Função D</a></li>
       </ul>
     </div>
+    <button onClick={chamadaBack}>Teste chamada</button>
+    </>
   );
 }
 
